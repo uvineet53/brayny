296806fac4323a139dbd6563962123083875991c
ridam <raggarwal1_be19@thapar.edu>
2022-01-27 15:16:19 +0530

    track

--- lib/backend/constants/testTrackData.dart
+++ lib/backend/constants/testTrackData.dart
@@ -1,20 +1,20 @@
 const mathTrack = [
   {
     'id': 0,
-    'title': 'Addition',
+    'title': 'ADDITION',
     'track': 'addition',
     'image':
         "https://digitallibrary.io/wp-content/uploads/2021/06/addition-300x300.png"
   },
   {
     'id': 1,
-    'title': 'Multiplication',
+    'title': 'MULTIPLICATION',
     'track': 'multiplication',
     'image': "https://imgur.com/Uiq4XYj.png"
   },
   {
     'id': 2,
-    'title': 'Squares',
+    'title': 'SQUARES',
     'track': 'squares',
     'image': "https://imgur.com/MFo2u0j.png"
   }
